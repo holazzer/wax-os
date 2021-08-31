2784a0ca7e7c5893bbe09218b7f75053c4f1cda9
holazzer <751727648@qq.com>
2021-08-31 23:16:39 +0800

    memory 2much!

--- kernel/memory.h
+++ kernel/memory.h
@@ -8,7 +8,7 @@ struct virtual_addr {
     uint32_t vaddr_start;       // 虚拟地址起始地址 
 };
 
-extern struct pool kernel_pool, user_pool; // ?
+extern struct pool kernel_pool, user_pool;
 
 void mem_init();
 
